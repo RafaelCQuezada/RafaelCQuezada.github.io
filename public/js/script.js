90c0f9b1147f6b12404042ab49cba510de84ba4f
Rafael Celis Quezada <rafaceque2001@gmail.com>
2023-05-11 13:55:32 -0400

    update

--- public/js/script.js
+++ public/js/script.js
@@ -116,20 +116,3 @@ localStorage.removeItem('theme')
 
 
 
-
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      } else {
-        entry.target.classList.remove('visible');
-      }
-    });
-  });
-  
-  const elements = document.querySelectorAll('.fade-in');
-  elements.forEach(el => {
-    observer.observe(el);
-  });
-  
